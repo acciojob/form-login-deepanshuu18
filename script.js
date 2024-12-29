f452df985d4b0cb637dd8a8df0cf384d1fa835f1
DEEPANSHU <deepanshufea07@gmail.com>
2024-12-29 22:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	 event.preventDefault();
+	 const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+	 alert(`${firstName} ${lastName}`);
 
 }
+document.getElementById('nameForm').addEventListener('submit', getFormvalue);
